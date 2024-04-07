a8d722916541d6dd93cb1ef7f533482cbb46c3e2
Atilla Altun <atilla.altun@ogr.iu.edu.tr>
2024-04-07 18:53:18 +0300

    navbar added

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Navbar />
+</template>
+
+<script>
+import Navbar from "./component/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap");
+html,
+body {
+  font-family: "Mulish", sans-serif;
+}
+</style>
